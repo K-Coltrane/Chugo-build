82ae1d35bbf58a251169cfae1d71817a6ca2bcba
Brandon Adofo <liciousintent@gmail.com>
2025-12-17 13:26:57 -0800

    Hide Android system navigation bar (immersive mode)

--- android/app/src/main/java/com/chugo_chop_chop/MainActivity.kt
+++ android/app/src/main/java/com/chugo_chop_chop/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.chugo_chop_chop
 
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    // RN template recommendation (supports react-native-screens)
+    super.onCreate(null)
+    hideSystemNavigationBar()
+  }
+
+  override fun onWindowFocusChanged(hasFocus: Boolean) {
+    super.onWindowFocusChanged(hasFocus)
+    if (hasFocus) hideSystemNavigationBar()
+  }
+
+  private fun hideSystemNavigationBar() {
+    try {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        val controller = window.insetsController ?: return
+        controller.hide(WindowInsets.Type.navigationBars())
+        controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+      } else {
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility =
+          View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+      }
+    } catch (_: Throwable) {
+      // Best-effort: don't crash if OEM ROM behaves unexpectedly
+    }
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
